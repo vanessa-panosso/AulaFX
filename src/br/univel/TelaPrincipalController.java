de24fac247172ecdd5c2a3dd27698b952fef3c2b
Vanessa Panosso <panossovanessa@gmail.com>
2016-03-22 20:45:28 -0300

    JavaFX

---
+++ src/br/univel/TelaPrincipalController.java
@@ -0,0 +1,22 @@
+package br.univel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TelaPrincipalController {
+	@FXML
+    private TextField campo;
+
+    @FXML
+    private Button botao;
+
+    @FXML
+    void executar(ActionEvent event) {
+
+    }
+ public void executar (){
+	 System.out.println(campo.getText());
+ }
+}
